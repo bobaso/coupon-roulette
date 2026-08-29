e238da0563772c65e3bded6b41062fef88db99c5
bobaso <yoshioka.mwork@gmail.com>
2026-08-29 11:51:03 +0900

    Delete Safari detection logic
    Removed Safari detection code from script.js.

--- script.js
+++ script.js
@@ -24,16 +24,7 @@ const coupons = [
 
 ];
 
-/* =========================================
-   Safari判定
-========================================= */
 
-const isSafari =
-    /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-if (isSafari) {
-    document.body.classList.add("safari-browser");
-}
 /* ==================================================
    HTML要素取得
 ================================================== */
